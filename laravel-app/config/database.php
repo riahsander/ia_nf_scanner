314123e438a27d89e1933422830e0a17d4c7f76f
Riah Sander <riahsander05@gmail.com>
2026-04-18 13:58:52 -0300

    Fix: Forçar SSL CA e desativar verify_server_cert

--- laravel-app/config/database.php
+++ laravel-app/config/database.php
@@ -59,9 +59,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+            PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', storage_path('certs/ca.pem')),
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         ],
 
         'mariadb' => [
